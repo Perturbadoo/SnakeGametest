be8564f90ca489673cfb2e0f55b92aca498902ad
Perturbadoo <andre.piratello@hotmail.com>
2022-12-15 21:57:32 -0300

    Mudar a cor da cabeça da cobra tiro arco iris

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.desktop.ScreenSleepEvent;
 import java.awt.event.*;
 import java.util.Random;
 
@@ -46,22 +47,34 @@ public class GamePanel extends JPanel implements ActionListener {
 
     public void draw(Graphics g) {
         if (running) {
+            /*
             for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
                 g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
                 g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
             }
+             */
             g.setColor(Color.red);
             g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 
             for (int i = 0; i < bodyParts; i++) {
                 if (i == 0) {
-                    g.setColor(Color.green);
+                     g.setColor(Color.green);
+                     /* COLOCAR A CABEÇA DA COBRA COM CORES ALEATORIAS
+                   g.setColor(new Color(random.nextInt(255))); */
                     g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
                 } else {
                     g.setColor(new Color(45, 180, 0));
+                     /* COLOCAR O CORPO DA COBRA COM CORES ALEATORIAS
+                    g.setColor(new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255))); */
                     g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
                 }
-            }
+                }
+
+
+            g.setColor(Color.white);
+            g.setFont(new Font("Impact", Font.BOLD, 40));
+            FontMetrics metrics = getFontMetrics(g.getFont());
+            g.drawString("Score: "+applesEaten, (SCREEN_WIDTH - metrics.stringWidth("Score: "+applesEaten))/2, g.getFont().getSize());
         }
         else{
             gameOver(g);
@@ -139,11 +152,25 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void gameOver(Graphics g) {
-        //Game over text
+
+        //Pontuação
+        g.setColor(Color.white);
+        g.setFont(new Font("Impact", Font.BOLD, 40));
+        FontMetrics metrics1 = getFontMetrics(g.getFont());
+        g.drawString("Score: "+applesEaten, (SCREEN_WIDTH - metrics1.stringWidth("Score: "+applesEaten))/2, g.getFont().getSize());
+
+        //texto game over
         g.setColor(Color.white);
         g.setFont(new Font("Impact", Font.BOLD, 75));
-        FontMetrics metrics = getFontMetrics(g.getFont());
-        g.drawString("Game Over", (SCREEN_WIDTH - metrics.stringWidth("Game Over"))/2, SCREEN_HEIGHT/2);
+        FontMetrics metrics2 = getFontMetrics(g.getFont());
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        g.drawString("Game Over", (SCREEN_WIDTH - metrics2.stringWidth("Game Over"))/2, SCREEN_HEIGHT/2);
+
+
 
     }
 
